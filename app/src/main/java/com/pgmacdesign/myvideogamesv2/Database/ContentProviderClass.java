ec2d9fa32affed9a5f56ce97a709d8e19a4cbd1d
pgmacdesign <pgmacdesign@gmail.com>
2015-02-10 21:27:11 -0800

    Version 2.0 Uploading. Working to reduce clutter, increase efficiency, and make it better code overall.

---
+++ app/src/main/java/com/pgmacdesign/myvideogamesv2/Database/ContentProviderClass.java
@@ -0,0 +1,88 @@
+package com.pgmacdesign.myvideogamesv2.Database;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+This class serves as the liason to manage the database data.
+ */
+public class ContentProviderClass extends ContentProvider {
+
+	DbHelper dbHelper;
+	SQLiteDatabase db;
+
+	private static final String AUTH = "com.pgmacdesign.myvideogames2.Database.ContentProviderClass";
+	public static final Uri LOC_URI = Uri.parse("content://" + AUTH + "/" + DbHelper.TABLE_NAME);
+
+	final static int MATCHING = 1;
+
+	private final static UriMatcher uriMatcher;
+	//Initiate uri matcher with static reference
+	static{
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(AUTH, DbHelper.TABLE_NAME, MATCHING);
+	}
+
+
+	public boolean onCreate() {
+		dbHelper = new DbHelper(getContext());
+		db = dbHelper.getWritableDatabase();
+
+		return true;
+	}
+
+
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		dbHelper.getWritableDatabase();
+
+		//If it matches, return 1
+		if (uriMatcher.match(uri) == MATCHING){
+			db.insert(dbHelper.TABLE_NAME, null, values);
+		}
+
+
+
+		//Insert data to database
+		getContext().getContentResolver().notifyChange(uri, null);
+
+		Log.d("Insert URI", "insert complete");
+		return null;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+		Cursor cursor;
+
+		db = dbHelper.getReadableDatabase();
+
+		cursor = db.query(DbHelper.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+
+		cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+		Log.d("query ", "query complete");
+		return cursor;
+
+	}
+
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		return 0;
+	}
+}
---
+++ app/src/main/java/com/pgmacdesign/myvideogamesv2/Database/DbHelper.java
@@ -0,0 +1,72 @@
+package com.pgmacdesign.myvideogamesv2.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ The database class. It will hold all of the data retrieved from the website that the user chose from
+ the list.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+
+	//Standard variables for the database functions
+	public static final String DATABASE_NAME = "videogamesdb2";
+	public static final String TABLE_NAME = "videogamestable";
+	public static final int DATABASE_VERSION = 3;
+	public static final String DROP_TABLE = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+
+	public static final String C_ID = "_id";
+	// Column with the game_id, IE 13053
+	public static final String COLUMN_GAME_ID = "game_id";
+	// Stored as an String, aliases for the game, IE FF7, FFVII
+	public static final String COLUMN_ALIASES = "aliases";
+	// Stored as a string, a short description of the game
+	public static final String COLUMN_DECK = "deck";
+	// Stored as an string, one of the image URLs
+	public static final String COLUMN_ICON_URL = "icon_url";
+	// Stored as an string, one of the image URLs
+	public static final String COLUMN_MEDIUM_URL = "medium_url";
+	// Stored as an string, the name of the game (IE Final Fantasy VII)
+	public static final String COLUMN_NAME = "name";
+	// Stored as an string, the original release date (IE 1997-01-31 00:00:00)
+	public static final String COLUMN_ORIGINAL_RELEASE_DATE = "original_release_date";
+	// Stored as a String, the platform name (IE Playstation)
+	public static final String COLUMN_PLATFORM_NAME = "platform_name";
+	// Stored as a String, the platform name abbreviation (IE PS1)
+	public static final String COLUMN_PLATFORM_ABBREVIATION = "platform_abbreviation";
+	// Stored as a boolean, checkbox for whether or not they have played it
+	public static final String COLUMN_PLAYED_CHECKBOX = "played_checkbox";
+	// Stored as a String, rating system using the android 'star' picker
+	public static final String COLUMN_RATING = "rating";
+
+	public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ("
+			+ C_ID + " integer primary key autoincrement, "
+			+ COLUMN_GAME_ID + " text,"
+			+ COLUMN_ALIASES + " text,"
+			+ COLUMN_DECK + " text,"
+			+ COLUMN_ICON_URL + " text,"
+			+ COLUMN_MEDIUM_URL + " text,"
+			+ COLUMN_NAME + " text,"
+			+ COLUMN_ORIGINAL_RELEASE_DATE + " text,"
+			+ COLUMN_PLATFORM_NAME + " text,"
+			+ COLUMN_PLATFORM_ABBREVIATION + " text,"
+			+ COLUMN_PLAYED_CHECKBOX + " text,"
+			+ COLUMN_RATING + " text"
+			+");";
+
+	public DbHelper(Context context){
+		super (context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(DROP_TABLE);
+		onCreate(db);
+	}
+}
